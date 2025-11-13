80a74653b3436c2ec736adf789b33b6652cd9710
davidfague <davidfague@live.com>
2025-11-13 16:26:39 -0600

    add fall back to exclude upper bound if not present.

--- src/ser_seg_dendevt.py
+++ src/ser_seg_dendevt.py
@@ -40,13 +40,17 @@ def ser_seg_dendevt(seg_df, **kwargs):
     evt_type = low_colname[0].split("_")[0]
 
     # if NMDA or Ca spikes, create start/stop series
-    if (evt_type == "nmda") or (evt_type == "ca"):
-        ser_func = lambda x: np.array(
-            ser_ss(x[low_colname[0]], x[up_colname[0]], **kwargs)
-        )
-    else:
+    try:
+        if (evt_type == "nmda") or (evt_type == "ca"):
+            ser_func = lambda x: np.array(
+                ser_ss(x[low_colname[0]], x[up_colname[0]], **kwargs)
+            )
+        else:
+            ser_func = lambda x: np.array(ser_pt(x[low_colname[0]], **kwargs))
+    except: # fall back to excluding upper bound if not present
         ser_func = lambda x: np.array(ser_pt(x[low_colname[0]], **kwargs))
 
+    # print(ser_func)
     # convert to binary series
     ser_temp = []
     for _, row in seg_df.iterrows():
